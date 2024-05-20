1e8f465a35551c7875ba25c401921a748b8c5fa0
Abhay Singh <78447760+Abhaysingh77@users.noreply.github.com>
2024-05-20 10:15:10 +0530

    fix style importing

--- components/quotation/quote.jsx
+++ components/quotation/quote.jsx
@@ -1,4 +1,4 @@
-import {Style} from './quote.module.css'
+import Style from './quote.module.css'
 export default function Quote() {
   return (
     <div className={Style.quote}> The health and well-being of our patients and their health care team will always be our priority, so we follow the best practices for cleanliness.</div>
